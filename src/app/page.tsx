45ba336db0ef3e27dd401693dae6e22ea4fed367
MayconCasttro <mayconcasttro@live.com>
2026-03-08 23:51:38 -0300

    fix: adicionar wrapper na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import ClientRoot from "@/components/ClientRoot";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Home() {
-  return <ClientRoot />;
+  return (
+    <div>
+      <ClientRoot />
+    </div>
+  );
 }
